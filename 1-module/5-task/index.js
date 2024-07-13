581fcca71e3c6ee61e16e00b37b7fe508d86fa76
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-07-13 08:58:41 +0300

    Создал функцию truncate для усечения строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
+/**
+ * Функция проверяет длину строки str и, если она превосходит maxlength, заменяет конец str на "…"
+ * @param {string} str
+ * @param {number} maxlength
+ * @returns {string} ту же строку, если усечение не требуется, либо
+ * усечённую строку, если длина исходной строки больше maxlength.
+ */
 function truncate(str, maxlength) {
-  // ваш код...
+  return str.length > maxlength ? str.slice(0, maxlength - 1) + "…" : str;
 }
