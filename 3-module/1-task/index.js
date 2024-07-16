c958947f5c7dd645a646db158ba9c9501b2b820a
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-07-16 16:48:19 +0300

    Выполнил Модуль 3 задача 1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,9 @@
-function namify(users) {
-  // ваш код...
+/**
+ * Функцию преобразует массив объектов в массив имён
+ * Функция возращает Array
+ * @param {Object[]} obj
+ * @return {Array}
+ */
+function namify(obj) {
+  return obj.map(item => item.name);
 }
