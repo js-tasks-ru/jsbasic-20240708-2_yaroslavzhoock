bfe869837b7b698499b49fe95ae4b55a7d3b300a
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-07-16 14:01:07 +0300

    Выполнил Модуль 1 задача 5, Модуль 2 задачи 1—3

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,10 @@
+/**
+ * Функцию возращает true, если в объекте нет свойств и false – если хоть одно свойство есть.
+ * @param {Object} obj
+ * @return {boolean}
+ */
+
 function isEmpty(obj) {
-  // ваш код...
+  for (let k in obj) return false;
+  return true;
 }
