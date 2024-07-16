ed9d017ef8b94aed55ad2122751d1c74d4082b34
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-07-16 19:25:09 +0300

    Выполнил Модуль 3 задача 2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
+/**
+ * Функция принимает массив чисел
+ * Функция не должна изменять исходный массив
+ * @param {Array} arr - Массив чисел
+ * @param {number} a
+ * @param {number} b
+ * @returns {Array} — возвращает новый массив элементов между a и b включая значения a и b
+ */
 function filterRange(arr, a, b) {
-  // ваш код...
+  return arr.filter(item => item >= a && item <= b);
 }
