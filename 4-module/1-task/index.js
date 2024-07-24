5a71fd6d7dc3bc14f2cf24efc871adbdf5f90c2a
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-07-24 21:40:22 +0300

    4-module/1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,16 @@
+/**
+ * Функция преобразует переданный массив в стандартный HTML.
+ * @param {Array} arr — Массив друзей.
+ * @return {Object} — Возвращает узел-элемент.
+ */
+
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement("ul");
+
+  for (let key of friends) {
+    let li = document.createElement("li");
+    li.innerHTML = `${key.firstName} ${key.lastName}`;
+    ul.append(li);
+  }
+  return ul;
 }
