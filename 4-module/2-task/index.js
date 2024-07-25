bc22af5d5d5a99f084d1054a2530d45c41ed8119
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-07-25 12:19:52 +0300

    4-module/2-task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,9 @@
-function makeDiagonalRed(table) {
-  // ваш код...
+/**
+ * Функция выделит красным цветом все ячейки в таблице по диагонали.
+ * @param {Element} e - table.
+ */
+function makeDiagonalRed(e) {
+  for (const [i, v] of Object.entries(e.rows)) {
+    v.cells[i].style.background = "red";
+  }
 }
