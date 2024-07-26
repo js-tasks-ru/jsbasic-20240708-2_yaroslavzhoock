9af5082113d1474d6bfcc3416367daaf29762f0e
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-07-26 09:26:45 +0300

    4-module/3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
+/**
+ * Метод устанавливает необходимые по условию атрибуты таблице
+ * @param {Element} table
+ */
 function highlight(table) {
-  // ваш код...
+  for (let tr of table.rows) {
+    for (let td of tr.cells) {
+      let tr = td.parentElement;
+
+      if (td.dataset.available == "true") tr.classList.add("available");
+      if (td.dataset.available == "false") tr.classList.add("unavailable");
+
+      !td.hasAttribute("data-available")
+        ? (tr.hidden = true)
+        : (tr.hidden = false);
+
+      if (td.innerHTML == "m") tr.classList.add("male");
+      if (td.innerHTML == "f") tr.classList.add("female");
+
+      if (+td.innerHTML < 18) tr.style.textDecoration = "line-through";
+    }
+  }
 }
