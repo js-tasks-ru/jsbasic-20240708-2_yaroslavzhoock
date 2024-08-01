dbe42a039c73bb5d53943d7d7c65c08fb02fdc41
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-08-01 18:35:07 +0300

    5-module/3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,29 @@
 function initCarousel() {
-  // ваш код...
+  const carouselWrapper = document.querySelector(".container"),
+    carouselArrowRight = carouselWrapper.querySelector(
+      ".carousel__arrow_right"
+    ),
+    carouselArrowLeft = carouselWrapper.querySelector(".carousel__arrow_left"),
+    carouselInner = carouselWrapper.querySelector(".carousel__inner");
+
+  let index = 0;
+  const transform = (i) =>
+    (carouselInner.style.transform = `translateX(-${
+      carouselInner.querySelector(".carousel__slide").offsetWidth * i
+    }px`);
+
+  carouselArrowLeft.style.display = "none";
+
+  carouselWrapper.onclick = (e) => {
+    if (e.target.closest(".carousel__arrow_right")) {
+      transform(++index);
+    }
+
+    if (e.target.closest(".carousel__arrow_left")) {
+      transform(--index);
+    }
+    carouselArrowRight.style.display =
+      index < carouselInner.children.length - 1 ? "" : "none";
+    carouselArrowLeft.style.display = index > 0 ? "" : "none";
+  };
 }
