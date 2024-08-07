b604be0d97459a1c6673672ea16e0df7fb22a3a4
Yaroslav Zhoock <zhoock@zhoock.ru>
2024-08-07 15:08:05 +0300

    6-module/1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,40 @@
  *      }
  *
  */
+
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = document.createElement("table");
+    this.elem.insertAdjacentHTML(
+      "afterbegin",
+      "<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th<th>Город</th><th></th></tr><tbody></tbody></thead>"
+    );
+
+    this.render();
+    this.onClick();
+  }
+
+  render() {
+    let tbody = this.elem.querySelector("tbody");
+    tbody.insertAdjacentHTML(
+      "afterbegin",
+      this.rows
+        .map(
+          (item) =>
+            `<tr><td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td><td><button>X</button></td></tr>`
+        )
+        .join("")
+    );
+
+    return this.elem;
+  }
+
+  onClick() {
+    this.elem.onclick = ({ target }) => {
+      let btn = target.closest("button");
+      if (btn) btn.closest("tr").remove();
+    };
   }
 }
